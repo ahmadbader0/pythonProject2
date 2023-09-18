a9f6f9170f76b7a6e7da5d75bda716382b6fd208
ahmad <123hooda2020@gmail.com>
2023-09-18 09:07:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def add( x , y ):
+    total = x + y
+    print(total)
+add(10,30)
+add(100,20)
+
+def add( x , y ):
+    total = x - y
+    print(total)
+add( 40 , 10)
+add(921, 1)
+
+
+def add(num1 ,num2):
+    total = num1 * num2
+    print(total)
+add(2,3)
+add(10,0)
